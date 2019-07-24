9594cd70fa824cae70b54faf76615ac279a1db36
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-07-24 19:12:49 -0400

    completed code for shared ptrs

--- 10_xx_pointers/main.cpp
+++ 10_xx_pointers/main.cpp
@@ -4,27 +4,36 @@
    1.) Raw pointers usage can lead to memory leaks -> not a good idea 
    2.) Use smart pointers instead 
    3.) Compare dot product results to eigen's
+*/
 
 #include <Eigen/Dense>
 #include <iostream>
+#include <memory>
 
 
 int main(void)
 {
+    // size of vectors
     size_t size = 10;
 
-    double *vector_ptr1, *vector_ptr2 = new double[size], new double[size];
-    double dot_product_ptr(0), dot_product_smrt(0), dot_product_eigen;
+    // Allocate memory for all pointers
+    double *vector_ptr1(new double[size]), *vector_ptr2(new double[size]);
+    std::unique_ptr<double[]> vector_smrt1(new double[size]), vector_smrt2(new double[size]);
+
+    // Instantiate eigen vectors
     Eigen::VectorXd vector_eigen1(size), vector_eigen2(size);
+    
+    // Initialize dot product variables (for storing results)
+    double dot_product_ptr(0), dot_product_smrt(0), dot_product_eigen;
 
     // Raw pointer
 
-    for(int i = 0; i < size; i++) {
+    for(size_t i = 0; i < size; i++) {
 
 	vector_ptr1[i] = i;
 	vector_ptr2[i] = i + 1;
 
-	dot_product += vector_ptr1[i] * vector_ptr2[i];
+	dot_product_ptr += vector_ptr1[i] * vector_ptr2[i];
     }
 
     delete[] vector_ptr1;
@@ -32,17 +41,17 @@ int main(void)
 
     // Smart pointer
 
-    for(int i = 0; i < size; i++) {
+    for(size_t i = 0; i < size; i++) {
 
-	vector_ptr1[i] = i;
-	vector_ptr2[i] = i + 1;
+	vector_smrt1[i] = i;
+	vector_smrt2[i] = i + 1;
 
-	dot_product += vector_ptr1[i] * vector_ptr2[i];
+	dot_product_smrt += vector_smrt1[i] * vector_smrt2[i];
     }
 
     // Eigen vectors
 
-    for(int i = 0; i < size; i++) {
+    for(size_t i = 0; i < size; i++) {
 
 	vector_eigen1[i] = i;
 	vector_eigen2[i] = i + 1;
@@ -50,7 +59,9 @@ int main(void)
 
     dot_product_eigen = vector_eigen1.dot(vector_eigen2);
 
-    std::cout << << "dot product (raw pointers) "  << dot_product << " , dot product (eigen) = " << dot_product_eigen << std::endl;
+    std::cout << "dot product (raw pointers) = "  << dot_product_ptr << std::endl << 
+    "dot product (smart pointers) = " << dot_product_smrt << std::endl << 
+    "dot product (eigen) = " << dot_product_eigen << std::endl;
 
     return 0;
 }
